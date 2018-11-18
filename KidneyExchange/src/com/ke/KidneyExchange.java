7399d01c16f38657d2bc83357fe9d4a1b44f9613
Jose C.M. Garcia-Vaso @jvaso <jvaso@grcooling.com>
2018-11-18 13:18:45 -0600

    Create shell of the Eclipse project.

---
+++ KidneyExchange/src/com/ke/KidneyExchange.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.ke;
+
+/**
+ * @author Jose Carlos Martinez Garcia-Vaso jcm3767 <carlosgvaso@gmail.com>
+ * @author Trevor Anderson
+ * @author Bryson Banks
+ * @author Alexander Hoganson
+ *
+ */
+public class KidneyExchange {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
