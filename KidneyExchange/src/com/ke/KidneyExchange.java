76b47c831be94685b061200e9ea6db06a21b20dd
Jose C.M. Garcia-Vaso @jvaso <jvaso@grcooling.com>
2018-11-21 11:43:08 -0600

    Added classes to represent weighted directed edges and directed graphs. Also, added shell for getNegativeCycles method.

--- KidneyExchange/src/com/ke/KidneyExchange.java
+++ KidneyExchange/src/com/ke/KidneyExchange.java
@@ -11,6 +11,76 @@ package com.ke;
  *
  */
 public class KidneyExchange {
+	// Class variables
+	private static int inf = 1000000000;// Infinity
+	
+	// Instance variables
+	private int L; 						// Cycle cap
+	private int[] w;					// Reduced edge weight vector
+	private Graph G;					// Reduced graph
+	
+	/**
+	 * A class to represent a weighted directed edge in graph.
+	 * 
+	 * Instance variables:
+	 * 	src		Edge source vertex.
+	 * 	dest	Edge destination vertex.
+	 * 	weight	Edge weight.
+	 */
+	private class Edge {
+		public int src, dest, weight;
+		
+		// Create an empty directed edge (src=dest=weight=0).
+		Edge() {
+			src = dest = weight = 0; 
+		}
+		
+		// Create a directed edge.
+		Edge(int src, int dest, int weight) {
+			this.src = src;
+			this.dest = dest;
+			this.weight = weight; 
+		}
+	}
+	
+	/**
+	 * A class to represent a connected, directed and weighted graph.
+	 * 
+	 * Instance variables:
+	 * 	V		Number or vertices.
+	 * 	E		Number of edges.
+	 * 	edge	Array with all the edges (Edge objects) in the graph.
+	 * 
+	 * To modify edge 0 in graph to point from vertex 2 to vertex 3 with a weight of 1:
+	 * 	graph.edge[0].src = 2; 
+	 * 	graph.edge[0].dest = 3; 
+	 * 	graph.edge[0].weight = 1; 
+	 */
+	private class Graph { 
+		public int V, E;
+		public Edge edge[];
+		
+		// Creates a graph with V vertices and E edges, where all the edges are empty.
+		Graph(int v, int e) {
+			V = v;
+			E = e;
+			edge = new Edge[e];
+			for (int i=0; i<e; ++i)
+				edge[i] = new Edge();
+		}
+	}
+	
+	/**
+	 * Find the negative weight cycles in graph G with a maximum length of L using a modified
+	 * version of the Bellman-Ford algorithm.
+	 * 
+	 * @param G	Reduced graph.
+	 * @param L	Maximum cycle length.
+	 * @param w	Reduced edge weight vector.
+	 */
+	private void getNegativeCycles(Graph G, int L, int[] w) {
+		
+	}
 
 	/**
 	 * @param args
